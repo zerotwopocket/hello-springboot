e60ee1e91f076e398867441b2d9558998fb8611e
02pocket <02pocket@anymail.com>
2022-09-20 23:22:05 +0800

    add request param message

--- src/main/java/com/zerotwopocket/hellospringboot/web/HelloController.java
+++ src/main/java/com/zerotwopocket/hellospringboot/web/HelloController.java
@@ -1,13 +1,14 @@
 package com.zerotwopocket.hellospringboot.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("hello")
-    public String hello(){
-       return "Hello Spring boot!";
+    public String hello(@RequestParam("message") String message){
+       return "Hello, "+ message+", from Spring boot!";
     }
 }
